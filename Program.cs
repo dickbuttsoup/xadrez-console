3c92f4d3684f35a7bc593cce3e042418ba4425d6
dickbuttsoup <kriuxo@gmail.com>
2020-02-22 19:32:33 -0300

    Criada a classe Posiçao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xadrez_Console.tabuleiro;
 
 namespace Xadrez_Console
 {
@@ -6,7 +7,9 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+            P = new Posicao(3, 4);
+            System.Console.WriteLine("Posição: " + P);
         }
     }
 }
